45a7cb58f4fc1591233eb51389f838f1ff887921
Tony Atkins <duhrer@gmail.com>
2013-02-03 22:28:25 +0100

    Updated overly generic filenames to avoid bundling conflicts with other libraries.

--- src/com/blogspot/tonyatkins/recorder/Constants.java
+++ src/com/blogspot/tonyatkins/recorder/Constants.java
@@ -22,10 +22,11 @@
  */
 package com.blogspot.tonyatkins.recorder;
 
+import android.os.Environment;
+
 public class Constants {
 	public static String TAG = "";
-	public static final String HOME_DIRECTORY = "/sdcard/com.blogspot.tonyatkins.archetype";
-	public static final String IMAGE_DIRECTORY = HOME_DIRECTORY + "/images";
-	public static final String SOUND_DIRECTORY = HOME_DIRECTORY + "/sounds";
+	public static final String IMAGE_DIRECTORY = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath();
+	public static final String SOUND_DIRECTORY = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC).getAbsolutePath();
 
 }
